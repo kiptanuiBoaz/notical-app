15ab4bf15fe454328f84355ce38fea4e066727f6
Kiptanui Boaz <boaserem022@gmail.com>
2023-12-03 19:37:17 +0300

    add starting files

---
+++ src/theme.js
@@ -0,0 +1,32 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+
+let theme = createTheme({
+    typography: {
+        fontFamily: "'Muli', sans-serif",
+        button: {
+            textTransform: "none",
+            textDecoration: "none",
+        },
+    },
+    palette: {
+        primary: {
+            main: '#0276aa', // Black color for primary
+        },
+        secondary: {
+            main: '#333333', // Dark grey color for secondary
+        },
+    },
+    components: {
+        MuiInputBase: {
+            styleOverrides: {
+                root: {
+                    borderRadius: 10, // change this line
+                },
+            },
+        },
+    },
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
