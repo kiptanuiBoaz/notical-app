e5676c2d9e7dc782873bfcec38b8227197120121
Kiptanui Boaz <boaserem022@gmail.com>
2024-01-10 11:13:26 +0300

    add error bounderies and loading state to auth module

---
+++ src/app/auth/signup/loading.jsx
@@ -0,0 +1,28 @@
+"use client"
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { Loading } from 'notiflix';
+
+export default function LoadingComponent() {
+    // Get the router object
+    const router = useRouter();
+
+    // Use useEffect to start and stop the loading animation on router events
+    useEffect(() => {
+        // Define a function to start the loading animation
+
+        Loading.dots({
+            svgColor: '#0276AA',
+            backgroundColor: 'rgba(0,0,0,0.4)',
+        });
+
+
+        // Remove event listeners on component unmount
+        return () => {
+            Loading.remove();
+        };
+    }, [router]);
+
+    // Return an empty component
+    return null;
+}
