5d97b72a36258d9a386573a0881179c83ac5ac91
Kiptanui Boaz <boaserem022@gmail.com>
2024-01-11 19:30:46 +0300

    fixed update last noton poll

--- src/components/ConnectCalendar.jsx
+++ src/components/ConnectCalendar.jsx
@@ -42,6 +42,7 @@ export const ConnectCalendar = () => {
 
                 </Box>
 
+
                 <Button
                     onClick={async () => {
                         await disconnectCalendar(user_id, email);
